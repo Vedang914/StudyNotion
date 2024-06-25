27fdec4da58fbce8f7ef8842b8a24995be85aa2b
vedang nagar <vedang914@gmail.com>
2024-06-25 20:55:36 +0530

    validation using pre using done

--- models/OTP.js
+++ models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { mailSender } = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
 
@@ -19,5 +20,24 @@ const OTPSchema = new mongoose.Schema({
     }
 
 });
+
+async function sendVerificationEmail(email,otp){
+
+    try{
+
+        const mailResponse = await mailSender(email,"Verification Mail from Study-Notion",otp)
+        console.log("Email sent successfully:",mailResponse);
+
+    } catch(error){
+        console.log("error occured while sending mail: ",error);
+        throw error;
+
+    }
+}
+
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP",OTPSchema);
