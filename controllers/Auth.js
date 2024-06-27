02074e2600bdcf839a0bb1a5663b054625ed4c31
vedang nagar <vedang914@gmail.com>
2024-06-27 20:10:44 +0530

    sendOTP completed

---
+++ controllers/Auth.js
@@ -0,0 +1,65 @@
+const OTP = require("../models/OTP");
+const User = require("../models/User");
+const otpGenerator = require("otp-generator");
+
+//send otp
+exports.sendOTP = async (req, res) => {
+  try {
+    // fetch email from req ki body
+    const { email } = req.body;
+
+    //check user already exist or not
+    const checkUserPresent = await User.findOne({ email });
+
+    if (checkUserPresent) {
+      return res.status(401).json({
+        success: false,
+        message: "User already exist",
+      });
+    }
+
+    // generate otp
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    console.log("OTP generated: ", otp);
+
+    let result = await OTP.findOne({ otp: otp });
+
+    while (result) {
+      otp = otpGenerator(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+
+      result = await OTP.findOne({ otp: otp });
+    }
+
+    const otpPayload = { email, otp };
+
+    //create an entry for OTP
+    const otpBody = await OTP.create(otpPayload);
+    console.log(otpBody);
+
+    res.status(200).json({
+      success: true,
+      message: "OTP sent successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//signup
+
+//login
+
+//change password
