02c14e92bc22aff9cbed4401443db4d23ec608f2
vedang nagar <vedang914@gmail.com>
2024-07-06 21:24:03 +0530

    auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,45 @@
+const user = require("User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.auth = async (req, res, next) => {
+  try {
+    //extract token
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    //token missing
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing",
+      });
+    }
+
+    //verify token
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+      req.user = decode;
+    } catch (error) {
+      //verification - issue
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
+
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while validating the token",
+    });
+  }
+};
+
+
+
